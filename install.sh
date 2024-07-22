850c877bc31461a39078ccc8d5253792ccf79db7
Monke But Cool <110347394+monkebutcool@users.noreply.github.com>
2024-07-22 20:52:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ read -p "" choice
 
 case $choice in
     [Yy])
-        pkg upgrade && pkg update && apt update && apt upgrade && pkg install wget && pkg install python && cd $HOME && mkdir MonkeMisc && wget https://monkebutcool.github.io/Termux-Stuff/MonkeMisc.sh && echo -e "\033[32m Successfully installed MonkeMisc!\033[0m"
+        pkg upgrade && pkg update && apt update && apt upgrade && pkg install wget && pkg install python && cd $HOME && mkdir MonkeMisc && wget https://raw.githubusercontent.com/monkebutcool/Termux-Stuff/main/monkemisc.sh && echo -e "\033[32m Successfully installed MonkeMisc!\033[0m"
         ;;
     [Nn])
         cd $HOME
